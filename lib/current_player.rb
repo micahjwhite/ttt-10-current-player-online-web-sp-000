dabe63df7487078872202f8bc58a763207c54dee
Micah White <micahjwhite@gmail.com>
2020-04-04 19:01:24 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,5 +3,6 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 0
   board.each do |element|
-    if element == "X" || element == "O"
+    if (element == "X" || element == "O")
+      counter += 1
 end
